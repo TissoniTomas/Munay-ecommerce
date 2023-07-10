f2b337a002c9ab7dc0ab93dd4e241d9e5b218f0e
Juan Ignacio Cansillieri <juancansi@gmail.com>
2023-07-10 00:26:48 -0300

    Rediseño navbar en mobile - Responsive arreglos en iconos y medidas

---
+++ src/assets/icons/index.js
@@ -0,0 +1,5 @@
+export { default as CartSVG } from './CartSVG.jsx';
+export { default as DarkSVG } from './DarkSVG.jsx';
+export { default as SearchSVG } from './CartSVG.jsx';
+export { default as MenuSVG } from './MenuSVG.jsx';
+export { default as Logo } from './LogoSVG.jsx';
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CartImg from '../assets/icons/CartSVG.jsx';
+import { CartSVG } from '../assets/icons';
 
 const CartWidget = ({ productsAmount }) => {
   return (
     <div>
-    <CartImg color='#fff' productsAmount={productsAmount} width={25}/>
+    < CartSVG color='#fff' productsAmount={productsAmount} className='w-[20px] sm:w-[25px]'/>
     <div className='absolute  right-[-8px] top-[-6px]'>
     <span className='flex items-center justify-center rounded-full bg-[#bdcf23] w-[15px] h-[15px] p-2 text-center text-xs font-bold'>{productsAmount}</span>
     </div>
