2335cb8f331b3c22f33b5de4330d91ba939b8ad0
Juan Ignacio Cansillieri <juancansi@gmail.com>
2023-07-27 23:30:50 -0300

    Preentrega2-Cansillieri

--- src/assets/icons/ExpandSVG.jsx
+++ src/assets/icons/ExpandSVG.jsx
@@ -5,7 +5,6 @@ const ExpandSVG = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     width={20}
     height={20}
-    className='mt-2'
     viewBox="0 -960 960 960"
     {...props}
   >
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 
+const linkClass = 'font-medium px-4 py-2 text-sm hover:bg-gray-100';
 const Categories = () => {
   return (
-    <ul className="sm:absolute sm:top-[95%] sm:left-[50%] sm:py-2 sm:z-2 sm:rounded-xl sm:bg-white sm:shadow-2xl text-white sm:dark:text-black sm:text-black">
-     <Link to={'categoria/productos/smartphones'}><li className="px-4 py-2 text-sm hover:bg-gray-100">Celulares</li></Link>
-     <Link to={'categoria/productos/laptops'}><li className="px-4 py-2 text-sm hover:bg-gray-50">Laptops</li></Link>
-     <Link to={'categoria/productos/skincare'}><li className="px-4 py-2 text-sm hover:bg-gray-50">Cremas</li></Link>
+    <ul className="sm:absolute sm:top-[95%] sm:left-[50%] sm:py-2 sm:z-2 sm:rounded-xl sm:bg-white sm:shadow-2xl text-black dark:text-white sm:dark:text-black sm:text-black">
+     <Link to={'categoria/productos/smartphones'}><li className={linkClass}>Celulares</li></Link>
+     <Link to={'categoria/productos/laptops'}><li className={linkClass}>Laptops</li></Link>
+     <Link to={'categoria/productos/skincare'}><li className={linkClass}>Cremas</li></Link>
    </ul>
   );
 };
